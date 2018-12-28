2ce067ff03b4cb37ea3a2d51b48537d981c40709
Peter Kreppein <pmkreppein@gmail.com>
2018-12-28 21:59:26 +0000

    Automatically backed up by Learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "sessions/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "application/hello", type: :view do
+
+  it "shows login link if not logged in" do
+    session[:name] = nil
+    render :template => 'application/hello.html.erb'
+    expect(rendered).to include '/login'
+  end
+
+  it "greets user by name if logged in" do
+    session[:name] = 'Pete'
+    render
+    expect(rendered).to include 'Hi, Pete'
+  end
 end
